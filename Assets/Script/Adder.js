b7c5fc83db8c1b8f17cf3f32b30bce9683b41398
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-10 21:48:59 -0500

    Added App
    First version of Alphabetizer GitHub has ever seen!

---
+++ Assets/Script/Adder.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+var Import : UnityEngine.UI.InputField;
+var Export : UnityEngine.UI.Text;
+
+var Character_Current : int; 
+var Word_Current : int;
+
+var Words = new Array ();
+
+var str : String;
+
+function Changer () {
+	Character_Current = 1; 
+	Word_Current = 1;
+	while (Character_Current <= Import.text.length) { 
+		if (Import.text.Substring(Character_Current - 1, 1) == " ") {
+			Word_Current++;
+		}else {
+			if (Words.length >= Word_Current + 1) {
+				Words[Word_Current] = Words[Word_Current].ToString() + Import.text.Substring(Character_Current - 1, 1); 
+			}else {
+				Words[Word_Current] = Import.text.Substring(Character_Current - 1, 1); 
+			}
+		}
+		Character_Current++; //Adds one to Character_Current
+	}
+
+	Words.Sort();
+	var Words_Sorted : String;
+	var Word_Current_Current : int = 1; //For putting the words in order
+	while (Word_Current >= Word_Current_Current) {
+		Words_Sorted = Words_Sorted + " " + Words[Word_Current_Current];
+		Word_Current_Current++;
+	}
+	Export.text = Words_Sorted;
+}
---
+++ Assets/Script/Finder.js
@@ -0,0 +1,24 @@
+﻿/*  -----------------------
+	Made by Andrew Friedman
+	3/28/17 10:47 PM
+	Shit is real
+	-----------------------  */
+	
+#pragma strict
+import System.IO;
+
+var sr : StreamReader;
+var line;
+var number2 : int;
+
+function Start () {
+	sr = new StreamReader("JustEveryWordEver.txt");
+	var number = Random.Range(1,144516);
+	while (number > number2) {
+		line = sr.ReadLine();
+		number2++;
+	}
+	Debug.Log(line);
+	Debug.Log(number);
+	sr.Close();
+} //BEFORE
